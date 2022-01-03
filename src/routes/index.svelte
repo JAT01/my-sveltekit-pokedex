508fae4029825d4a6897ab716198e42caa1be464
JAT <j.a.tera@gmail.com>
2022-01-03 17:03:05 +0000

    premier commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { pokemon } from '../stores/pokestore';
+	import PokemanCard from '../components/pokemanCard.svelte';
+
+    let searchTerm = "";
+    let filteredPokemon = [];
+
+    $:{
+        console.log(searchTerm);
+    }
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Pokemon">
+
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each $pokemon as pokeman}
+		<PokemanCard {pokeman} />
+	{/each}
+</div>
